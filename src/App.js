6dd824fe9cec42e8293ded97420524ee7a175e9b
maxpsz <maximiliano@upshow.tv>
2022-07-04 10:01:03 -0300

    Add assets + choice logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
+import { useState, useEffect } from 'react';
+
+import Card from './components/Card';
+
 import './App.css';
 
+const cardImages = [
+  { src: '/img/helmet-1.png' },
+  { src: '/img/potion-1.png' },
+  { src: '/img/ring-1.png' },
+  { src: '/img/scroll-1.png' },
+  { src: '/img/shield-1.png' },
+  { src: '/img/sword-1.png' }
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+
+  const shuffleCard = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  const handleChoice = card => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(prevTurns => prevTurns + 1);
+  };
+
+  useEffect(() => {
+    if (!choiceOne || !choiceTwo) return;
+
+    console.log(choiceOne.src === choiceTwo.src ? 'Those cards match' : 'Those cards do not match');
+
+    resetTurn();
+  }, [choiceOne, choiceTwo]);
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Memory Game</h1>
+      <button onClick={shuffleCard}>New Game</button>
+
+      <div className='card-grid'>
+        {cards.map(card => (
+          <Card key={card.id} card={card} handleChoice={handleChoice} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import './Card.css';
+
+const Card = ({ card, handleChoice }) => {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+
+  return (
+    <div className='card'>
+      <div>
+        <img className='front' src={card.src} alt='card front' />
+        <img className='back' src='img/cover.png' onClick={handleClick} alt='card back' />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
